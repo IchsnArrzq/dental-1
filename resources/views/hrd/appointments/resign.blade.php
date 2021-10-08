1bab4fbaf6b6c955acc1c402c5b79a7c7ef6c817
IchsnArrzq <ichsanarrizqi090@gmail.com>
2021-10-08 16:48:06 +0700

    resign

--- resources/views/hrd/appointments/resign.blade.php
+++ resources/views/hrd/appointments/resign.blade.php
@@ -11,7 +11,7 @@
     <div class="col-md-12">
         <div class="card shadow">
             <div class="card-header">
-                <a href="{{ route('hrd.appointments.resign') }}" class="btn btn-info">(Dokter Resign) Tindakan yang belum selesai : {{ $tindakan}}</a>
+                <a href="{{ url()->previous() }}" class="btn btn-warning">Back</a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -21,8 +21,6 @@
                                 <th>Id</th>
                                 <th>No Booking</th>
                                 <th>Dokter</th>
-                                <th>Dokter Pengganti</th>
-                                <th>Tanggal Pengganti</th>
                                 <th>Pasien</th>
                                 <th>Status</th>
                                 <th>Tindakan</th>
@@ -30,32 +28,28 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($booking as $book)
+                            @foreach($tindakan as $book)
                             <tr>
                                 <td>{{ $book->id }}</td>
-                                <td><span>{{ $book->no_booking }}</span></td>
-                                <td>{{ $book->dokter->name }}</td>
-                                <td>{{ $book->dokter_pengganti->name ?? 'Kosong' }}</td>
-                                <td>{{ $book->tanggal_pengganti ?? 'Kosong' }}</td>
-                                <td>{{ $book->pasien->nama }}</td>
-                                <td><span class="custom-badge status-{{ $book->status->warna }}">{{ $book->status->status }}</span></td>
+                                <td><span>{{ $book->booking->no_booking }}</span></td>
+                                <td><span class="custom-badge status-red">{{ $book->dokter->name }}</span></td>
+                                <td>{{ $book->booking->pasien->nama }}</td>
+                                <td><span class="custom-badge status-{{ $book->booking->status->warna }}">{{ $book->booking->status->status }}</span></td>
                                 <td>
-                                    @foreach($book->tindakan as $row)
                                     <ul class="list-unstyled">
                                         <li>
-                                            @if($row->status)
-                                            <span class="custom-badge status-green">{{ $row->item->nama_barang }} - {{ $row->dokter->name }}</span>
+                                            @if($book->booking->status)
+                                            <span class="custom-badge status-green">{{ $book->item->nama_barang }} - {{ $book->dokter->name }}</span>
                                             @else
-                                            <span class="custom-badge status-red">{{ $row->item->nama_barang }} - {{ $row->dokter->name }}</span>
+                                            <span class="custom-badge status-red">{{ $book->item->nama_barang }} - {{ $book->dokter->name }}</span>
                                             @endif
                                         </li>
                                     </ul>
-                                    @endforeach
                                 </td>
                                 <td>
                                     <div class="btn-group">
-                                        <a href="{{ route('hrd.appointments.show', $book->id) }}" class="btn btn-success">Image</a>
-                                        <a href="{{ route('hrd.appointments.edit', $book->id) }}" class="btn btn-warning">Edit</a>
+                                        <a href="{{ route('hrd.appointments.show', $book->booking->id) }}" class="btn btn-success">Image</a>
+                                        <a href="{{ route('hrd.appointments.edit', $book->booking->id) }}" class="btn btn-warning">Edit</a>
                                     </div>
                                 </td>
                             </tr>
